6522325c8ba5c9e3e6bfd89ad3592a07090ba0f4
Joaquim Alves <jafn92@gmail.com>
2023-03-29 22:54:04 -0300

    fix: change relationship many travels to one user

--- app/Models/Travel.ts
+++ app/Models/Travel.ts
@@ -1,13 +1,16 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, belongsTo, BelongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
 
 export default class Travel extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @hasMany(() => User)
-  public user: HasMany<typeof User>
+  @column()
+  public user_id: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 
   @column()
   public destination: string
